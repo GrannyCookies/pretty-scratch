35a64af4b1d1dc64d87bab08fe3ae1509575eb36
Alex Bates <16batesa@gmail.com>
2015-02-27 16:29:28 +0000

    Create pretty-scratch.php

---
+++ pretty-scratch.php
@@ -0,0 +1,9 @@
+<?php
+//http://scratchextproxy.x10.mx/pretty-scratch.php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/css');
+
+$url = 'https://raw.githubusercontent.com/GrannyCookies/pretty-scratch/master/main.css';
+
+echo file_get_contents($url);
+?>
