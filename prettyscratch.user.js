201fb113e4c2507d1be70791caefa4f549a8960b
Alex Bates <16batesa@gmail.com>
2015-03-01 17:34:39 +0000

    added version no and setClipboard

--- prettyscratch.user.js
+++ prettyscratch.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         Pretty Scratch
 // @namespace    https://github.com/GrannyCookies/pretty-scratch
-// @version      0.2
+// @version      1.0
 // @description  Prettifies Scratch
 // @author       GrannyCookies
 // @match        http://scratch.mit.edu/*
-// @run-at document-start
+// @grant		 GM_setClipboard
+// @grant    	 unsafeWindow
+// @run-at		 document-start
 // ==/UserScript==
 
 installCSS = function(url) {
@@ -19,6 +21,12 @@ installJS = function(url) {
     var link = window.document.createElement('script');
     link.src = url + '?d=' + new Date();
     document.getElementsByTagName("HEAD")[0].appendChild(link);
+};
+
+unsafeWindow.prettyScratchVer = 1.0;
+unsafeWindow.setClipboard = function(str) {
+    // set clipboard swag
+	GM_setClipboard(str);
 };
 
 installCSS('http://scratchextproxy.x10.mx/pretty-scratch.php');
