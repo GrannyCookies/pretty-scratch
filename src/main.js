1e22ad01ac6609ab7d030b5d99eb07e083c5e15e
Alex Bates <16batesa@gmail.com>
2015-03-01 20:48:42 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -44,7 +44,7 @@ window.onload = function() {
     $('#comments .comment .info .content').each(function() {
       var text = $(this).html();
       var i = 0;
-      while(i<=emoji.length && i<10) {
+      while(i<=emoji.length) {
         text = text.replace(emoji[i], '<img src="https://raw.githubusercontent.com/GrannyCookies/pretty-scratch/gh-pages/emoji/' + image[i] + '.png" class="ps-emoji">');
         i++;
       }
