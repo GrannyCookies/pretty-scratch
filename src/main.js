2ee316f0698127ca01d7811f0d78a87e9b6a53e4
Alex Bates <16batesa@gmail.com>
2015-03-01 11:35:43 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,8 +24,8 @@ function doPrettyScratch() {
 
 // add emoji
 function doEmoji() {
-  var emoji = [':P', ':(', ':)', ':D', '8)', ' :/', ':o', ':3', ':|', ':O', '-_-', ';)', 'xD', 'XD', 'D:<', '>:('];
-  var image = ['tounge', 'aww', 'smile', 'happy', 'cool', 'er', 'speechless', 'whistle', 'straight', 'what', 'youdontsay', 'wink', 'xd', 'xd', 'killer', 'angry'];
+  var emoji = [':P', '>:(', ':)', ':D', '8)', ' :/', ':o', ':3', ':|', ':O', '-_-', ';)', 'xD', 'XD', 'D:<', ':('];
+  var image = ['tounge', 'angry', 'smile', 'happy', 'cool', 'er', 'speechless', 'whistle', 'straight', 'what', 'youdontsay', 'wink', 'xd', 'xd', 'killer', 'aww'];
   
   $('#comments .comment .info .content').each(function() {
     var text = $(this).html();
